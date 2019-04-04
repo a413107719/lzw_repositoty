7689185602181f6a9b9db77ce9eae022e9303c38
lzw19910 <42457098+lzw19910@users.noreply.github.com>
2019-04-04 11:02:32 +0800

    111

---
+++ 3.RandomForest/NRandomForest.py
@@ -0,0 +1,48 @@
+# conding=utf-8
+import numpy as np
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+ 
+train_data = pd.read_csv("D:/datacsv2.csv")
+ 
+ 
+#def harmonize_data(titanic):
+    # 填充空数据 和 把string数据转成integer表示
+    # titanic['SI'] = titanic['SI'] * 10000
+    # titanic["Age"] = titanic["Age"].fillna(titanic["Age"].median())
+ 
+    # titanic.loc[titanic["Sex"] == "male", "Sex"] = 0
+    # titanic.loc[titanic["Sex"] == "female", "Sex"] = 1
+ 
+    # titanic["Embarked"] = titanic["Embarked"].fillna("S")
+ 
+    # titanic.loc[titanic["Embarked"] == "S", "Embarked"] = 0
+    # titanic.loc[titanic["Embarked"] == "C", "Embarked"] = 1
+    # titanic.loc[titanic["Embarked"] == "Q", "Embarked"] = 2
+ 
+    # titanic["Fare"] = titanic["Fare"].fillna(titanic["Fare"].median())
+ 
+    # return titanic
+ 
+#train_data = harmonize_data(train)
+
+
+predictors = ["TR","TCH","FZZ","MZ","SLH","TRJ","TRS","SJD","JMD","DM","PW","ELE","ASP","SLP"]
+results = []
+sample_leaf_options = list(range(1, 500, 3))
+n_estimators_options = list(range(1, 1000, 5))
+groud_truth = train_data['SI'][601:]
+print("sss")
+for leaf_size in sample_leaf_options:
+    for n_estimators_size in n_estimators_options:
+        alg = RandomForestClassifier(min_samples_leaf=leaf_size, n_estimators=n_estimators_size, random_state=50)
+        alg.fit(train_data[predictors][:600], train_data['SI'][:600])
+        predict = alg.predict(train_data[predictors][601:])
+        # 用一个三元组，分别记录当前的 min_samples_leaf，n_estimators， 和在测试数据集上的精度
+        results.append((leaf_size, n_estimators_size, (groud_truth == predict).mean()))
+        # 真实结果和预测结果进行比较，计算准确率
+        print((groud_truth == predict).mean())
+
+# 打印精度最大的那一个三元组
+print("End".max(results, key=lambda x: x[2]))
+print("End")
