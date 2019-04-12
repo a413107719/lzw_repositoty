d731455f27c82fb5530aaa23880817aa80121bdd
lzw19910 <lzw19910@users.noreply.github.com>
2019-04-12 23:58:04 +0800

    201904122241

--- 3.MachineLearning/RandomForest.py
+++ 3.MachineLearning/RandomForest.py
@@ -1,63 +1,164 @@
-# -*- coding:utf-8 -*-
 import numpy as np
 import pandas as pd
-from sklearn import svm
+from sklearn import tree
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import confusion_matrix as con
+from sklearn.metrics import zero_one_loss
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import AdaBoostClassifier
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
+import prettytable
+from prettytable import PrettyTable
 
 
+# 因子重要性判断
+def top_variable_importance():
+    # 对训练好的随机森林，完成重要性评估
+    importances = forest.feature_importances_
+    # print(importances)
+    x_columns = traindata_all.columns[1:len(traindata_all.columns)-1]
+    indices = np.argsort(importances)[::-1]
+    # print(indices)
+    np.argsort(x_columns,)
+    list=[]
+    print('-- Top Variable Importance --')
+    table3 = PrettyTable(['序号', '评价特征', '重要性'])
+    for f in range(X_train.shape[1]):
+        table3.add_row([f+1, feat_labels[indices[f]], round(importances[indices[f]], 2)])
+    print(table3)
+    print()
+    # 筛选变量（选择重要性比较高的变量）
+    threshold = 0.15
+    x_selected = X_train[:, importances > threshold]
 
-# 导入数据集
-data = pd.read_csv("D:/data23.csv")
-predictors = ["TR", "TCH", "FZZ", "MZ", "SLH", "TRJ", "TRS", "SJD", "JMD", "DM", "PW", "ELE", "ASP", "SLP"]
-x = data[predictors]
-y = data['SI']
-
-# 划分训练集和测试集数据
-train_data, test_data = train_test_split(data, test_size=0.2)   # 参数
-
-# 随机森林框架参数选取
-# n_estimators:森林中决策树的个数（有的地方称为ntree），默认是10。一般来说n_estimators太小，容易欠拟合，n_estimators太大，计算量会太大
-# oob_score:是否采用袋外样本来评估模型的好坏。默认值False,推荐设置为True,因为袋外分数反应了一个模型拟合后的泛化能力。
-# criterion:CART树划分时对特征的评价标准。分类RF默认是基尼系数gini,回归RF默认是均方差mse。
-# bootstrap: boolean类型取值，表示是否采用有放回式的抽样方式，默认True
-
-# 随机森林决策树参数选取
-# max_features：寻求最佳分割时的考虑的特征数量（有的地方称为mtry），即特征数达到多大时进行分割。
-    # int: 特征绝对数，float: 总特征数的百分比， "auto": 等于sqrt(N)， "sqrt": 同等于"auto"， "log2": 对于log2(N)，None: 等于N
-# max_depth：决策树最大深度，int或者None。一般数据少或者特征少的时候可以不管这个值。
-# min_samples_split：分割内部节点所需的最少样本数量，int或float，默认为2，若样本不大，可忽略
-# min_samples_leaf：叶子节点最少样本数，int或float，默认为1，若样本不大，可忽略
-# min_weight_fraction_leaf:能成为叶子节点的条件是该节点对应的实例数和总样本数的比值，至少大于这个值，默认是0
-# max_leaf_nodes：最大叶子节点数，通过限制最大叶子节点数防止过拟合。如果加了限制，算法会建立在最大叶子节点数内最优的决策树。默认是"None”，若特征不多，可忽略
-# min_impurity_split：节点划分最小不纯度，这个值限制了决策树的增长，如果某节点的不纯度（基于基尼系数，均方差）小于这个阈值，则该节点不再生成子节点，一般不推荐改动默认值
-
-
-target='SI'
-IDcol = 'ID'
-print('表征特征统计：')
-print(data['SI'].value_counts())
-print()
-
-# 对比不同分类树数量，误差率大小，以确定最优n_estimators数量
-for i in range(9):
-    tree_number = 20+10*i
-    rf0 = RandomForestClassifier(oob_score=True, random_state=10, max_features=10, n_estimators=tree_number)
-    rf0.fit(x, y)
-    print(tree_number)
-    print(rf0.oob_score_)
-# todo:绘制“分类树数量对应误差率折线图”
+    # 可视化
+    import matplotlib.pyplot as plt
+    plt.figure(figsize=(10, 6))
+    plt.title("数据集中各个特征的重要程度", fontsize=18)
+    plt.ylabel("import level", fontsize=15, rotation=90)
+    plt.rcParams['font.sans-serif'] = ["SimHei"]
+    plt.rcParams['axes.unicode_minus'] = False
+    for i in range(x_columns.shape[0]):
+        plt.bar(i, importances[indices[i]], color='orange', align='center')
+        plt.xticks(np.arange(x_columns.shape[0]), list, rotation=90, fontsize=12)
+    plt.show()
 
 
+# 分类树数量和误差率的关系
+def findbest_n_estimators(max_trees_num):
+    learning_rate = 1
+
+    dt_stump = DecisionTreeClassifier(max_depth=1, min_samples_leaf=1)
+    dt_stump.fit(X_train, y_train)
+    dt_stump_err = 1.0 - dt_stump.score(X_test, y_test)
+
+    dt = DecisionTreeClassifier(max_depth=9, min_samples_leaf=1)
+    dt.fit(X_train, y_train)
+    dt_err = 1.0 - dt.score(X_test, y_test)
+
+    ada_discrete = AdaBoostClassifier(
+        base_estimator=dt_stump,
+        learning_rate=learning_rate,
+        n_estimators=max_trees_num,
+        algorithm="SAMME")
+    ada_discrete.fit(X_train, y_train)
+
+    ada_real = AdaBoostClassifier(
+        base_estimator=dt_stump,
+        learning_rate=learning_rate,
+        n_estimators=max_trees_num,
+        algorithm="SAMME.R")
+    ada_real.fit(X_train, y_train)
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+
+    ax.plot([1, max_trees_num], [dt_stump_err] * 2, 'k-',
+            label='Decision Stump Error')
+    ax.plot([1, max_trees_num], [dt_err] * 2, 'k--',
+            label='Decision Tree Error')
+
+    ada_discrete_err = np.zeros((max_trees_num,))
+    for i, y_pred in enumerate(ada_discrete.staged_predict(X_test)):
+        ada_discrete_err[i] = zero_one_loss(y_pred, y_test)
+
+    ada_discrete_err_train = np.zeros((max_trees_num,))
+    for i, y_pred in enumerate(ada_discrete.staged_predict(X_train)):
+        ada_discrete_err_train[i] = zero_one_loss(y_pred, y_train)
+
+    ada_real_err = np.zeros((max_trees_num,))
+    for i, y_pred in enumerate(ada_real.staged_predict(X_test)):
+        ada_real_err[i] = zero_one_loss(y_pred, y_test)
+
+    ada_real_err_train = np.zeros((max_trees_num,))
+    for i, y_pred in enumerate(ada_real.staged_predict(X_train)):
+        ada_real_err_train[i] = zero_one_loss(y_pred, y_train)
+
+    ax.plot(np.arange(max_trees_num) + 1, ada_discrete_err,
+            label='Discrete AdaBoost Test Error',
+            color='red')
+    ax.plot(np.arange(max_trees_num) + 1, ada_discrete_err_train,
+            label='Discrete AdaBoost Train Error',
+            color='blue')
+    ax.plot(np.arange(max_trees_num) + 1, ada_real_err,
+            label='Real AdaBoost Test Error',
+            color='orange')
+    ax.plot(np.arange(max_trees_num) + 1, ada_real_err_train,
+            label='Real AdaBoost Train Error',
+            color='green')
+
+    ax.set_ylim((0.0, 0.5))
+    ax.set_xlabel('max_trees_num')
+    ax.set_ylabel('error rate')
+
+    leg = ax.legend(loc='upper right', fancybox=True)
+    leg.get_frame().set_alpha(0.7)
+
+    plt.show()
 
 
-# todo:对比不同随机特征(max_features)取值,误差率大小，以确定最优随机特征个数
-# todo:交叉验证:得到信息增益最大的那棵树
-# todo:数据验证：用混淆矩阵判断模型的泛化程度
-# todo:因子重要性判断：得到因子重要性排序表
-# todo:单因子影响程度
+if __name__ == '__main__':
+    # 获取数据
+    traindata_all = pd.read_csv("D:/data24.csv")
+    traindata_all.columns = ["ID", "TR", "TCH", "FZZ", "MZ", "SLH", "TRJ", "TRS", "SJD", "JMD", "DM", "PW", "ELE", "ASP", "SLP", "SI"]
 
+    # 将数据集分为训练集和测试集
+    X, y = traindata_all.iloc[:, 1:len(traindata_all.columns) - 1].values, traindata_all.iloc[:, len(traindata_all.columns) - 1].values     # 选择评价特征和标签特征
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
 
-# 绘图：
-# 可视化随机森林中的一棵树： https://www.jianshu.com/p/58815b3e761c
-# https://blog.csdn.net/xinshucredit/article/details/88538017
+    # 打印评价特征和标签特征
+    feat_labels = traindata_all.columns[1:len(traindata_all.columns)-1]
+    print('\n' + '--- Data summary ---')
+    # info_url = traindata_all.info()
+    print('评价特征名称为：'+str([x for x in feat_labels]) )
+    print('标签特征名称为：' + str(traindata_all.columns[len(traindata_all.columns)-1])+'\n')
+    print('--- 标签特征统计表 ---')
+    yValue_table = traindata_all['SI'].value_counts()
+    table2 = PrettyTable(['值', '数量', '占比'])
+    for i in range(len(yValue_table)):
+        value = yValue_table.index[i]
+        number = yValue_table[value]
+        table2.add_row([value, number, round(number/len(X), 2)])
+    print(table2)
+    print()
+
+    # 建立模型
+    forest = RandomForestClassifier(n_estimators=10000, random_state=0, n_jobs=-1)  # 实例化
+    forest.fit(X_train, y_train)   # 用训练集数据训练模型
+    result = forest.fit(X_train, y_train).predict(X_test)
+    # 打印混淆矩阵
+    print('------------ 随机森林模型混淆矩阵 ------------')
+    table = con(y_test, result)
+    table_x = PrettyTable(['实际类别', '预测适宜', '预测不适宜', '分类误差率'])
+    table_x.add_row(["适宜", table[0][0], table[0][1], round(table[0][1] / table[0][0], 2)])
+    table_x.add_row(["不适宜", table[1][0], table[1][1], round(table[1][1] / table[1][0], 2)])
+    print(table_x)
+    score = forest.score(X_test, y_test)    # 测试准确率
+    print('准确率： ' + str(round(score, 2)) + '\n')
+
+    # 分类树数量和误差率的关系
+    findbest_n_estimators(100)
+
+    # 因子重要性判断
+    top_variable_importance()
