416e3e3731c68dd088fd7c19abaac17942e5c10c
lzw19910 <42457098+lzw19910@users.noreply.github.com>
2019-04-15 13:53:07 +0800

    0411

--- 3.MachineLearning/RandomForest.py
+++ 3.MachineLearning/RandomForest.py
@@ -11,8 +11,27 @@ from sklearn.model_selection import train_test_split
 from prettytable import PrettyTable
 
 
+# 打印评价特征和标签特征
+def print_featurelable():
+    feat_labels = traindata_all.columns[1:len(traindata_all.columns) - 1]
+    print('\n' + '--- Data summary ---')
+    # info_url = traindata_all.info()
+    print('评价特征名称为：' + str([x for x in feat_labels]))
+    print('标签特征名称为：' + str(traindata_all.columns[len(traindata_all.columns) - 1]) + '\n')
+    print('--- 标签特征统计表 ---')
+    yvalue_table = traindata_all['SI'].value_counts()
+    table2 = PrettyTable(['值', '数量', '占比'])
+    for i in range(len(yvalue_table)):
+        value = yvalue_table.index[i]
+        number = yvalue_table[value]
+        table2.add_row([value, number, round(number / len(X), 2)])
+    print(table2)
+    print()
+
+
 # 因子重要性判断
-def top_variable_importance():
+def top_variable_importance(forest):
+    feat_labels = traindata_all.columns[1:len(traindata_all.columns) - 1]
     # 对训练好的随机森林，完成重要性评估
     importances = forest.feature_importances_
     # print(importances)
@@ -43,6 +62,14 @@ def top_variable_importance():
         plt.bar(i, importances[indices[i]], color='orange', align='center')
         plt.xticks(np.arange(x_columns.shape[0]), list, rotation=90, fontsize=12)
     plt.show()
+
+
+# 建立模型
+def build_model():
+    forest = RandomForestClassifier(n_estimators=500, random_state=0, n_jobs=-1)  # 实例化
+    forest.fit(X_train, y_train)  # 用训练集数据训练模型
+    result = forest.fit(X_train, y_train).predict(X_test)
+    return forest, result
 
 
 # 分类树数量和误差率的关系
@@ -117,6 +144,7 @@ def findbest_n_estimators(max_trees_num):
 
     plt.show()
 
+
 # 不同max_features取值对应误差大小
 def findbest_maxfeatures():
     # 据此修改https://scikit-learn.org/dev/auto_examples/ensemble/plot_ensemble_oob.html#sphx-glr-auto-examples-ensemble-plot-ensemble-oob-py
@@ -150,6 +178,20 @@ def findbest_maxfeatures():
     print(table3)
     print()
 
+    # 打印混淆矩阵
+
+
+def print_mxjuzhen(result, forest):
+    print('------------ 随机森林模型混淆矩阵 ------------')
+    table = con(y_test, result)
+    table_x = PrettyTable(['实际类别', '预测适宜', '预测不适宜', '分类误差率'])
+    table_x.add_row(["适宜", table[0][0], table[0][1], round(table[0][1] / table[0][0], 2)])
+    table_x.add_row(["不适宜", table[1][0], table[1][1], round(table[1][1] / table[1][0], 2)])
+    print(table_x)
+    score = forest.score(X_test, y_test)    # 测试准确率
+    print('准确率： ' + str(round(score, 2)) + '\n')
+
+
 if __name__ == '__main__':
     # 获取数据
     traindata_all = pd.read_csv("D:/data24.csv")
@@ -160,40 +202,19 @@ if __name__ == '__main__':
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
 
     # 打印评价特征和标签特征
-    feat_labels = traindata_all.columns[1:len(traindata_all.columns)-1]
-    print('\n' + '--- Data summary ---')
-    # info_url = traindata_all.info()
-    print('评价特征名称为：'+str([x for x in feat_labels]) )
-    print('标签特征名称为：' + str(traindata_all.columns[len(traindata_all.columns)-1])+'\n')
-    print('--- 标签特征统计表 ---')
-    yValue_table = traindata_all['SI'].value_counts()
-    table2 = PrettyTable(['值', '数量', '占比'])
-    for i in range(len(yValue_table)):
-        value = yValue_table.index[i]
-        number = yValue_table[value]
-        table2.add_row([value, number, round(number/len(X), 2)])
-    print(table2)
-    print()
+    print_featurelable()
 
     # 建立模型
-    forest = RandomForestClassifier(n_estimators=500, random_state=0, n_jobs=-1)  # 实例化
-    forest.fit(X_train, y_train)   # 用训练集数据训练模型
-    result = forest.fit(X_train, y_train).predict(X_test)
+    forest, result = build_model()
+
     # 打印混淆矩阵
-    print('------------ 随机森林模型混淆矩阵 ------------')
-    table = con(y_test, result)
-    table_x = PrettyTable(['实际类别', '预测适宜', '预测不适宜', '分类误差率'])
-    table_x.add_row(["适宜", table[0][0], table[0][1], round(table[0][1] / table[0][0], 2)])
-    table_x.add_row(["不适宜", table[1][0], table[1][1], round(table[1][1] / table[1][0], 2)])
-    print(table_x)
-    score = forest.score(X_test, y_test)    # 测试准确率
-    print('准确率： ' + str(round(score, 2)) + '\n')
+    print_mxjuzhen(result, forest)
 
     # 分类树数量和误差率的关系
-    # findbest_n_estimators(100)
+    findbest_n_estimators(100)
 
     # 不同max_features取值对应误差大小
     findbest_maxfeatures()
 
     # 因子重要性判断
-    top_variable_importance()
+    top_variable_importance(forest)
