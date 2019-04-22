d88a75f5c2494e0ec3a9ec70583381772bb7d04d
lzw19910 <42457098+lzw19910@users.noreply.github.com>
2019-04-22 18:04:44 +0800

    0411

--- 3.MachineLearning/RandomForest.py
+++ 3.MachineLearning/RandomForest.py
@@ -23,10 +23,10 @@ def print_featurelable():
     feat_labels = traindata_all.columns[1:len(traindata_all.columns) - 1]
     print('\n' + '--- Data summary ---')
     # info_url = traindata_all.info()
-    print('评价特征名称为：' + str([x for x in feat_labels]))
-    print('标签特征名称为：' + str(traindata_all.columns[len(traindata_all.columns) - 1]) + '\n')
+    print('特征因子：' + str([x for x in feat_labels]))
+    print('标签因子：' + str(traindata_all.columns[len(traindata_all.columns) - 1]) + '\n')
     print('--- 标签特征统计表 ---')
-    yvalue_table = traindata_all['SI'].value_counts()
+    yvalue_table = traindata_all[str(traindata_all.columns[len(traindata_all.columns) - 1])].value_counts()
     table2 = PrettyTable(['值', '数量', '占比'])
     for i in range(len(yvalue_table)):
         value = yvalue_table.index[i]
@@ -36,80 +36,11 @@ def print_featurelable():
     print()
 
 
-# 交叉验证
-def cross_validation():
-    k_range = range(1, 31)
-    cv_scores = []		# 用来放每个模型的结果值
-    for n in k_range:
-        knn = KNeighborsClassifier(n)   # knn模型，这里一个超参数可以做预测，当多个超参数时需要使用另一种方法GridSearchCV
-        scores = cross_val_score(knn, X_train, y_train, cv=10, scoring='accuracy')  # cv：选择每次测试折数  accuracy：评价指标是准确度,可以省略使用默认值
-        cv_scores.append(scores.mean())
-    max_score = max(cv_scores)
-    max_index = cv_scores.index(max_score)
-
-    # 绘出图形
-    plt.plot(k_range, cv_scores)
-    mpl.rcParams['font.sans-serif'] = ['SimHei']
-    plt.title("交叉验证", fontsize=18)
-    plt.xlabel('K_range')
-    plt.ylabel('Accuracy')		# 通过图像选择最好的参数
-    plt.show()
-    best_knn = KNeighborsClassifier(n_neighbors=max_index)	 # 选择最优的K=3传入模型
-    best_knn.fit(X_train, y_train)			# 训练模型
-    print('------ 交叉验证 ------')
-    table4 = PrettyTable(['最优K值', '最优评分'])
-    table4.add_row([max_index, round(best_knn.score(X_test, y_test), 4)])
-    print(table4)
-    print()
-
-# 因子重要性判断
-def top_variable_importance(forest):
-    feat_labels = traindata_all.columns[1:len(traindata_all.columns) - 1]
-    # 对训练好的随机森林，完成重要性评估
-    importances = forest.feature_importances_
-    # print(importances)
-    x_columns = traindata_all.columns[1:len(traindata_all.columns)-1]
-    indices = np.argsort(importances)[::-1]
-    # print(indices)
-    np.argsort(x_columns,)
-    list = []
-    print('-- 所有变量影响程度排序 --')
-    table3 = PrettyTable(['序号', '评价特征', '重要性(%)'])
-    for f in range(X_train.shape[1]):
-        table3.add_row([f+1, feat_labels[indices[f]], round(importances[indices[f]]*100, 2)])
-        list.append(feat_labels[indices[f]])
-    print(table3)
-    print()
-    # 筛选变量（选择重要性比较高的变量）
-    threshold = 0.15
-    x_selected = X_train[:, importances > threshold]
-
-    # 可视化
-    import matplotlib.pyplot as plt
-    plt.figure(figsize=(10, 6))
-    plt.title("数据集中各个特征的重要程度", fontsize=18)
-    plt.ylabel("import level", fontsize=15, rotation=90)
-    plt.rcParams['font.sans-serif'] = ["SimHei"]
-    plt.rcParams['axes.unicode_minus'] = False
-    for i in range(x_columns.shape[0]):
-        plt.bar(i, importances[indices[i]], color='orange', align='center')
-        plt.xticks(np.arange(x_columns.shape[0]), list, rotation=90, fontsize=12)
-    plt.show()
-
-
-# 建立模型
-def build_model():
-    forest = RandomForestClassifier(n_estimators=500, random_state=0, n_jobs=-1)  # 实例化
-    forest.fit(X_train, y_train)  # 用训练集数据训练模型
-    result = forest.fit(X_train, y_train).predict(X_test)
-    return forest, result
-
-
-# 分类树数量和误差率的关系
+# 不同n_estimators取值对应误差大小
 def findbest_n_estimators(max_trees_num):
     learning_rate = 1
 
-    dt_stump = DecisionTreeClassifier(max_depth=1, min_samples_leaf=1)
+    dt_stump = DecisionTreeClassifier(max_depth=1, min_samples_leaf=1, max_features=2)
     dt_stump.fit(X_train, y_train)
     dt_stump_err = 1.0 - dt_stump.score(X_test, y_test)
 
@@ -168,7 +99,7 @@ def findbest_n_estimators(max_trees_num):
             label='Real AdaBoost Train Error',
             color='green')
 
-    ax.set_ylim((0.0, 0.5))
+    ax.set_ylim((0.23, 0.3))
     ax.set_xlabel('n_estimators')
     ax.set_ylabel('error rate')
 
@@ -210,16 +141,86 @@ def findbest_maxfeatures():
     print()
 
 
+# 建立模型
+def build_model():
+    forest = RandomForestClassifier(n_estimators=350, random_state=0, n_jobs=-1, max_features =2)  # 实例化
+    forest.fit(X_train, y_train)  # 用训练集数据训练模型
+    result = forest.fit(X_train, y_train).predict(X_test)
+    return forest, result
+
+
+# 交叉验证
+def cross_validation():
+    k_range = range(1, 31)
+    cv_scores = []		# 用来放每个模型的结果值
+    for n in k_range:
+        knn = KNeighborsClassifier(n)   # knn模型，这里一个超参数可以做预测，当多个超参数时需要使用另一种方法GridSearchCV
+        scores = cross_val_score(knn, X_train, y_train, cv=10, scoring='accuracy')  # cv：选择每次测试折数  accuracy：评价指标是准确度,可以省略使用默认值
+        cv_scores.append(scores.mean())
+    max_score = max(cv_scores)
+    max_index = cv_scores.index(max_score)
+
+    # 绘出图形
+    plt.plot(k_range, cv_scores)
+    mpl.rcParams['font.sans-serif'] = ['SimHei']
+    plt.title("交叉验证", fontsize=18)
+    plt.xlabel('K_range')
+    plt.ylabel('Accuracy')		# 通过图像选择最好的参数
+    plt.show()
+    best_knn = KNeighborsClassifier(n_neighbors=max_index)	 # 选择最优的K=3传入模型
+    best_knn.fit(X_train, y_train)			# 训练模型
+    print('------ 交叉验证 ------')
+    table4 = PrettyTable(['最优K值', '最优评分'])
+    table4.add_row([max_index, round(best_knn.score(X_test, y_test), 4)])
+    print(table4)
+    print()
+
+
 # 打印混淆矩阵
 def print_mxjuzhen(result, forest):
     print('------------ 随机森林模型混淆矩阵 ------------')
     table = con(y_test, result)
     table_x = PrettyTable(['实际类别', '预测适宜', '预测不适宜', '分类误差率(%)'])
     table_x.add_row(["适宜", table[0][0], table[0][1], round(table[0][1] / table[0][0]*100, 2)])
-    table_x.add_row(["不适宜", table[1][0], table[1][1], round(table[1][0] / table[1][1]*100, 2)])
+    table_x.add_row(["不适宜", table[1][0], table[1][1], round(table[1][1] / table[1][0]*100, 2)])
     print(table_x)
     score = forest.score(X_test, y_test)    # 测试准确率
     print('准确率： ' + str(round(score, 2)) + '\n')
+
+
+# 因子重要性判断
+def top_variable_importance(forest):
+    feat_labels = traindata_all.columns[1:len(traindata_all.columns) - 1]
+    # 对训练好的随机森林，完成重要性评估
+    importances = forest.feature_importances_
+    # print(importances)
+    x_columns = traindata_all.columns[1:len(traindata_all.columns)-1]
+    indices = np.argsort(importances)[::-1]
+    # print(indices)
+    np.argsort(x_columns,)
+    list = []
+    print('-- 所有变量影响程度排序 --')
+    table3 = PrettyTable(['序号', '评价特征', '重要性(%)'])
+    for f in range(X_train.shape[1]):
+        table3.add_row([f+1, feat_labels[indices[f]], round(importances[indices[f]]*100, 2)])
+        list.append(feat_labels[indices[f]])
+    print(table3)
+    print()
+    # 筛选变量（选择重要性比较高的变量）
+    threshold = 0.15
+    x_selected = X_train[:, importances > threshold]
+
+    # 可视化
+    import matplotlib.pyplot as plt
+    plt.figure(figsize=(10, 6))
+    plt.title("数据集中各个特征的重要程度", fontsize=18)
+    plt.ylabel("import level", fontsize=15, rotation=90)
+    plt.rcParams['font.sans-serif'] = ["SimHei"]
+    plt.rcParams['axes.unicode_minus'] = False
+    for i in range(x_columns.shape[0]):
+        plt.bar(i, importances[indices[i]], color='orange', align='center')
+        plt.xticks(np.arange(x_columns.shape[0]), list, rotation=90, fontsize=12)
+    plt.show()
 
 
 # 画出决策树
@@ -248,16 +249,16 @@ def draw_tree():
     print("D:/out0416.png")
 
 
-
 if __name__ == '__main__':
     # 设置参数
     import arcpy
-    csv_route = arcpy.GetParameterAsText(0)  # 设置训练数据路径，例如D:/data24.csv。格式必须为csv,第一列是ID，最后一列是标签。
-    testsize = arcpy.GetParameterAsText(1)   # 设置测试数据占比，例如0.3。格式为float。
+    # csv_route = arcpy.GetParameterAsText(0)  # 设置训练数据路径，例如D:/data24.csv。格式必须为csv,第一列是ID，最后一列是标签。
+    # testsize = arcpy.GetParameterAsText(1)   # 设置测试数据占比，例如0.3。格式为float。
 
 
     # 获取数据
-    traindata_all = pd.read_csv("D:/data24.csv")
+    # traindata_all = pd.read_csv("D:/data24.csv")
+    traindata_all = pd.read_csv("F:/data.csv")
 
     # 将数据集分为训练集和测试集
     X, y = traindata_all.iloc[:, 1:len(traindata_all.columns) - 1].values, traindata_all.iloc[:, len(traindata_all.columns) - 1].values     # 选择评价特征和标签特征
@@ -265,6 +266,12 @@ if __name__ == '__main__':
 
     # 打印评价特征和标签特征
     print_featurelable()
+
+    # 不同n_estimators取值对应误差大小
+    findbest_n_estimators(1000)
+
+    # 不同max_features取值对应误差大小
+    findbest_maxfeatures()
 
     # 建立模型
     forest, result = build_model()
@@ -274,12 +281,6 @@ if __name__ == '__main__':
 
     # 打印混淆矩阵
     print_mxjuzhen(result, forest)
-
-    # 分类树数量和误差率的关系
-    findbest_n_estimators(100)
-
-    # 不同max_features取值对应误差大小
-    findbest_maxfeatures()
 
     # 因子重要性判断
     top_variable_importance(forest)
