3680ddcb887123e0e1f2aa8181a2d24fc5157b2f
lzw19910 <lzw19910@qq.com>
2019-08-19 10:12:09 +0800

    1

--- 2.WebScraping/图吧.py
+++ 2.WebScraping/图吧.py
@@ -1,6 +1,7 @@
 import requests, re, time, openpyxl, os
 from bs4 import BeautifulSoup
 from retrying import retry
+import random
 
 
 # 找到各类型链接
@@ -14,11 +15,11 @@ def kinds_data(point_url):
     for i in selects:
         titles.append(i.getText())
         kinds_urls.append(i.get("href"))
-    print(titles)
-    print(kinds_urls)
+    # print(titles)
+    # print(kinds_urls)
 
     for title, url in zip(titles, kinds_urls):
-        print(title, url)
+        print('\n', title, url)
         pack_imformation(url, title) #打包信息
         time.sleep(3)
         print("------------------------------------休息3秒-------------------------------------")
@@ -43,24 +44,37 @@ def pack_imformation(kinds_url,kinds_name):
     for name, pointlink in zip(namelist, linklist):
         # print( name, pointlink)
         try:
-            province, city, lattitude, longitude = point_data(pointlink)
-            print(name, kinds_name, province, city, lattitude, longitude)
-            kinds_all_list.append([name, kinds_name, province, city, lattitude, longitude]) 
-        except:
-            pass
-        
+            if point_data(pointlink) is None:
+                print("将不填入信息")
+            else:
+                province, city, lattitude, longitude = point_data(pointlink)
+                print(name, kinds_name, province, city, lattitude, longitude, pointlink)
+                kinds_all_list.append([name, kinds_name, province, city, lattitude, longitude])
+        except Exception as e:
+            print(e)
+            print("出错，重新请求")
+            time.sleep(10)
+            pack_imformation(kinds_url, kinds_name)
+
 
 # 找到每个点的信息
 def point_data(point_url):
+    sleeptime = random.randint(1, 2)
+    time.sleep(sleeptime)
     web_data = requests.get(point_url)
     soup = BeautifulSoup(web_data.text,'lxml')
     names = soup.find_all('meta')
     text = names[2].get('content')       #province=四川;city=自贡;coord=104.76247,29.37363
     textregex = re.compile(r'province=(\S\S);city=(\S\S);coord=(\d+\W\d+),(\d+\W\d+)')
     mo = textregex.search(text)
-    province,city,lattitude,longitude = mo.groups()
-    # print(province,city,lattitude,longitude,sep='\n')
-    return province,city,lattitude,longitude
+    if mo is not None:
+        province, city, lattitude, longitude = mo.groups()
+        # print(province,city,lattitude,longitude,sep='\n')
+        return province, city, lattitude, longitude
+    else:
+        print("此链接没信息：" + point_url)
+        return None
+
 
 # 写入Excel
 def write2excel():
@@ -91,5 +105,11 @@ main_url = 'http://poi.mapbar.com/liuzhou/'
 kinds_all_list = []
 kinds_data(main_url)
 
-# kinds_all_list = [[1, 2, 3, 4, 5, 6], ['a', 'b', 'c', 'd', 'e', 'f']]
-# write2excel()
+
+# url = 'http://poi.mapbar.com/liuzhou/MAPIXBYBFEXBXBSOXMXBC'
+# url = 'http://poi.mapbar.com/liuzhou/MAPIXBYBFEXBXBSOXMXBC'
+# point_data(url)
+
+# url= 'http://poi.mapbar.com/liuzhou/940/'
+# name = '风景名胜test'
+# pack_imformation(url, name)
