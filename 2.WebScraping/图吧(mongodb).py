60479401cbf39431dede3fe66b6a259a1d0fdda9
lzw19910 <lzw19910@qq.com>
2020-01-03 09:53:25 +0800

    图吧信息爬取_存入mongodb

--- 2.WebScraping/图吧(mongodb).py
+++ 2.WebScraping/图吧(mongodb).py
@@ -27,9 +27,9 @@ def get_kindscrapt():
 # 找到需要爬取的数据大类名称和链接
 @retry(stop_max_attempt_number=3)
 def kinds_data(mainurl):
-    global proxies
+    global proxie_ip
     try:
-        web_data = requests.get(mainurl, proxies=proxies)  # 找到目标页面所有分类名称和地址
+        web_data = requests.get(mainurl, proxies=proxie_ip)  # 找到目标页面所有分类名称和地址
         soup = BeautifulSoup(web_data.text, "html.parser")
         selects = soup.select('body > div.sort.cl > div > div > a')
         kind_names = []
@@ -40,28 +40,34 @@ def kinds_data(mainurl):
 
         # 从模板excel提取需要爬取的数据类
         scraptlist = get_kindscrapt()
+        print(scraptlist)
+        print(kind_names)
+
         kinds_willscrapt_name = []
         kinds_willscrapt_url = []
         for kind_name, kind_url in zip(kind_names, kind_urls):
             if kind_name in scraptlist:
                 kinds_willscrapt_name.append(kind_name)
                 kinds_willscrapt_url.append(kind_url)
-        return kind_names, kind_urls
+        return kinds_willscrapt_name, kinds_willscrapt_url
 
     except Exception as e:
         print(e, mainurl)
         print("kinds_data出错，重新请求")
         time.sleep(5)
-        proxies = get_proxies()
+        # 切换代理
+        change_proxies()
         kinds_data(mainurl)
+
+
 
 
 # 得到一个类所有点的链接
 @retry(stop_max_attempt_number=3)
 def kind_allpoints_data(kinds_url):
-    global proxies
+    global proxies_list, proxie_ip
     try:
-        web_data = requests.get(kinds_url, proxies=proxies)
+        web_data = requests.get(kinds_url, proxies=proxie_ip)
         soup = BeautifulSoup(web_data.text, "html.parser")
         selects = soup.select('div.sort.cl > div.sortC > dl > dd > a')
 
@@ -73,16 +79,17 @@ def kind_allpoints_data(kinds_url):
     except Exception as e:
         print(e, kinds_url)
         print("get_allpoints_namelink出错，重新请求")
-        time.sleep(2)
-        proxies = get_proxies()
+        time.sleep(5)
+        # 切换代理
+        change_proxies()
         kind_allpoints_data(kinds_url)
 
 
 # 找到所有点的信息
 def get_pointimformation(namelist, linklist, kind_name):
-    global kinds_all_list
+    global kinds_all_list, proxie_ip
     print('\n' + "重新验证代理是否成功：")
-    print(proxies)
+    print(proxie_ip)
     kinds_all_list = []
     count = 0
     for name, pointurl in zip(namelist, linklist):
@@ -99,12 +106,13 @@ def get_pointimformation(namelist, linklist, kind_name):
         else:
             print(name + '：已经存在')
 
+
 # 找到每个点的具体信息
 @retry(stop_max_attempt_number=3)
 def point_data(name, point_url, kind_name):
-    global proxies
+    global proxies_list, proxie_ip
     try:
-        web_data = requests.get(point_url, proxies=proxies)
+        web_data = requests.get(point_url, proxies=proxie_ip)
         soup = BeautifulSoup(web_data.text, "html.parser")
         names = soup.find_all('meta')
         text = names[2].get('content')  # province=四川;city=自贡;coord=104.76247,29.37363
@@ -128,68 +136,66 @@ def point_data(name, point_url, kind_name):
         print(e, point_url)
         print("point_data出错，重新请求")
         time.sleep(7)
-        proxies = get_proxies()
+        # 切换代理
+        change_proxies()
         point_data(name, point_url, kind_name)
 
 
-# def point_data(name, point_url, kind_name):
-#     global proxies
-#     web_data = requests.get(point_url, proxies=proxies)
-#     soup = BeautifulSoup(web_data.text, "html.parser")
-#     names = soup.find_all('meta')
-#     text = names[2].get('content')  # province=四川;city=自贡;coord=104.76247,29.37363
-#     textregex = re.compile(r'province=(\S\S);city=(\S\S);coord=(\d+\W\d+),(\d+\W\d+)')
-#     mo = textregex.search(text)
-#     if mo is not None:
-#         province, city, lattitude, longitude = mo.groups()
-#         kinds_all_list.append([name, kind_name, province, city, lattitude, longitude])
-#         print(str(len(kinds_all_list)) + ':' + str([name, kind_name, province, city, lattitude, longitude]))
-#
-#         # 写入mongodb
-#         pointimformation = [name, kind_name, province, city, lattitude, longitude]
-#         dict = {'name': pointimformation[0], 'kind': pointimformation[1], 'province': pointimformation[2],
-#                 'city': pointimformation[3], 'longitude': pointimformation[4], 'latitude': pointimformation[5]}
-#         mycolumn.update(dict, dict, True)
-#
-#     else:
-#         print("此链接没信息：" + point_url)
-
-
-# 获取代理IP，并合成一个proxise
-def get_proxies():
+@retry(stop_max_attempt_number=3)
+def get_10_proxies():
     try:
-        url = "http://http.tiqu.alicdns.com/getip3?num=1&type=1&pro=&city=0&yys=0&port=1&time=1&ts=0&ys=0&cs=0&lb=1&sb=0&pb=4&mr=1&regions=&gm=4"  # 从代理网站上获取的url
+        url = "http://http.tiqu.alicdns.com/getip3?num=10&type=1&pro=&city=0&yys=0&port=1&pack=79679&ts=0&ys=0&cs=0&lb=1&sb=0&pb=4&mr=1&regions=&gm=4"  # 从代理网站上获取的url
         page = requests.get(url)
         ip = page.text
-        proxy_ip = 'http://' + ip.split('\r')[0]
-        proxies = {'http': proxy_ip}      # {'http': 'http://58.218.214.141:4549'}
-        print("代理抓取成功: " + proxy_ip)   # 代理列表抓取成功: http://{"code":111,"data":[],"msg":"请2秒后再试","success":false}
-        if proxy_ip == 'http://{"code":111,"data":[],"msg":"请2秒后再试","success":false}':
-            raise Exception('未能获取有效代理地址')
+        print(ip)
+        if ip == '{"code": 115, "data": [], "msg": "请重新提取", "success": false}':
+            raise Exception('"code": 115, "data": [], "msg": "请重新提取"')
+        elif ip == 'http://{"code":111,"data":[],"msg":"请2秒后再试","success":false}':
+            raise Exception('"code":111,"data":[],"msg":"请2秒后再试"')
+        else:
+            iplist = ip.split("\r\n")
+            iplist.pop()
+        proxies =[]
+        for i in iplist:
+            proxie = 'http://' + i
+            proxies.append(proxie)
         return proxies
     except Exception as e:
+        print("未能获取有效代理地址:")
         print(e)
         time.sleep(5)
-        get_proxies()
+        get_10_proxies()
 
+
+# 切换代理
+def change_proxies():
+    global proxies_list, proxie_ip
+    test = random.sample(proxies_list,1)[0]
+    ip = random.sample(proxies_list,1)[0]
+    proxie_ip = {'http': ip}
+    print("proooooooooooooo")
+    print(proxie_ip)
 
 def main():
-    global quelist, proxies
+    global quelist, proxies_list
     quelist = []
-    proxies = get_proxies()
+    proxies_list = get_10_proxies()
+    change_proxies()
+    print(1)
     kindscraptname, kindscrapturl = kinds_data(main_url)  # 找到需要爬取的数据大类名称和链接
+    print(2)
     for i in range(len(kindscraptname)):
         kindname = kindscraptname[i]
         kindurl = kindscrapturl[i]
         try:   # 找到每一个类的所有点的名称和链接
             kind_allpoints_name, kind_allpoints_url = kind_allpoints_data(kindurl)
-            print(kind_allpoints_name, kind_allpoints_url)
+            print(kindname, kind_allpoints_name, kind_allpoints_url)
 
             # 判断此类数据在mongodb中的数量是否一致，如果一致则跳过分类
             kind_number = len(kind_allpoints_name)
             findresult = len(list(mycolumn.find({"kind": kindname})))
             ratio = findresult/kind_number
-            print(kind_number, findresult, ratio)
+            print(kindname, kind_number, findresult, ratio)
             if ratio > 0.96:
                 print(kindname + "已爬取完成，将爬取下一类型数据" + "\n")
             else:
@@ -197,8 +203,10 @@ def main():
                 get_pointimformation(kind_allpoints_name, kind_allpoints_url, kindname)
 
         except Exception as e:
+            print("main()error")
             print(e)
             main()
+    print("成功爬取所有数据！！！")
 
 
 if __name__ == '__main__':
