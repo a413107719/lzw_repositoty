60b51bf9a5ee4ec74cec881b1497cc87a0ee9097
lzw19910 <lzw19910@users.noreply.github.com>
2020-04-02 15:04:42 +0800

    111

---
+++ 0.ITS toolbox/excel根据特征字段合并（网络分析用）.py
@@ -0,0 +1,42 @@
+import geohash
+import pandas as pd
+
+
+infid = "IN_FID"
+nearfc = "NEAR_FC"
+nearfid = 'NEAR_FID'
+poi = "poi"
+mid = "道路中点"
+
+# 构建结果dataframe
+outputdataframe = pd.DataFrame(columns=(infid, poi, mid))
+
+
+excelfile = 'F:\\测试数据\\网络拆数据\\POI_道路中点.xls'
+data = pd.read_excel(excelfile)
+unique_infid = data[infid].unique()
+
+for value in unique_infid:
+    # value = 610
+    sub_df = data [data[infid].isin([value])]
+    sub_df_point = sub_df[sub_df[nearfc].isin([poi])]
+    sub_df_mid = sub_df[sub_df[nearfc].isin([mid])]
+    datalist = value, list(sub_df_point[nearfid]), list(sub_df_mid[nearfid])
+    print(value, list(sub_df_point[nearfid]), list(sub_df_mid[nearfid]))
+
+    dic = [{infid: value, poi: str(list(sub_df_point[nearfid])).split('[')[1].split(']')[0], mid: str(list(sub_df_mid[nearfid])).split('[')[1].split(']')[0]}]
+    # print(dic)
+    # print()
+    outputdataframe = outputdataframe.append(dic, ignore_index=True)
+    # break
+
+# print(outputdataframe)
+# print(outputdataframe[poi])
+
+outputdataframe1 = outputdataframe.drop(poi, axis=1).join(outputdataframe[poi].str.split(',', expand=True).stack().reset_index(level=1, drop=True).rename(poi))
+print(outputdataframe1)
+
+outputroute = excelfile.split('.')[0] + 'output' + '.xls'
+writer = pd.ExcelWriter(outputroute)
+outputdataframe1.to_excel(writer, float_format='%.5f')
+writer.save()
